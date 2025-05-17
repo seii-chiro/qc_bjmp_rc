af1d9b7a643b613a488ab4d4fdf55f3059775cbd
John Rhay <javier.johnrhay@gmail.com>
2025-05-17 10:04:28 +0800

    feat: add terms and conditions

--- src/pages/visitor_management/VisitorID.tsx
+++ src/pages/visitor_management/VisitorID.tsx
@@ -171,27 +171,31 @@ const VisitorID = () => {
                                 </div>
                             </div>
                             <div className='flex absolute top-[25%] left-0 w-full h-full p-5'>
-                                <div className='flex-1 border-4 border-black h-[75%]'>
-                                    <h1>TERMS AND CONDITION</h1>
-                                    <pre>
-                                        This Identfication card is only valid for verified
-                                        visitors for Jail Visitation on Bureau of Jail
-                                        Management and Penology - Quezon City Jail
-                                        Male dorm premises only.
-                                    </pre>
-                                    <pre>
-                                        This card is used for entering/ exiting the jail
-                                        facility and for the identification of the card
-                                        holder.
-                                    </pre>
-                                    <pre>
-                                        This identification card is non-transferrable.
-                                    </pre>
-                                    <pre>
-                                        Report immediately upon the loss of this card
-                                        and if found, please return this ID card to BJMP
-                                        QCJMD Gate Security.
-                                    </pre>
+                                <div className='flex-1 border-4 border-black h-[75%] text-sm'>
+                                    <div className='p-2'>
+                                        <h1 className='w-full text-center font-semibold text-lg'>TERMS AND CONDITION</h1>
+                                        <div className='w-full flex flex-col gap-2 pt-4 text-justify'>
+                                            <p>
+                                                This Identfication card is only valid for verified
+                                                visitors for Jail Visitation on Bureau of Jail
+                                                Management and Penology - Quezon City Jail
+                                                Male dorm premises only.
+                                            </p>
+                                            <p>
+                                                This card is used for entering/ exiting the jail
+                                                facility and for the identification of the card
+                                                holder.
+                                            </p>
+                                            <p>
+                                                This identification card is non-transferrable.
+                                            </p>
+                                            <p>
+                                                Report immediately upon the loss of this card
+                                                and if found, please return this ID card to BJMP
+                                                QCJMD Gate Security.
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div className="flex flex-col justify-between h-[70%] w-10 pt-24">
                                     <span
