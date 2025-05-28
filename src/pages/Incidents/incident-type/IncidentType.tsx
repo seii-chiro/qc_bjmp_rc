54c8a40d821c8f4cdafebe02891bfeea32596aab
John Rhay <javier.johnrhay@gmail.com>
2025-05-28 22:08:51 +0800

    fix: fix errors and add edit, delete, sort, and filter function.

--- src/pages/Incidents/incident-type/IncidentType.tsx
+++ src/pages/Incidents/incident-type/IncidentType.tsx
@@ -1,61 +1,65 @@
-import { IncidentTypeResponse } from "@/lib/issues-difinitions";
-import { getUser } from "@/lib/queries";
-import { deleteIncidentType, getIncidentType } from "@/lib/query";
+import { deleteIncidentType, getIncidentCategory, getIncidentType } from "@/lib/query";
 import { useTokenStore } from "@/store/useTokenStore";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { message } from "antd";
+import { Button, Input, message, Modal } from "antd";
 import Table, { ColumnsType } from "antd/es/table";
+import { Plus } from "lucide-react";
 import { useState } from "react";
+import IncidentForm from "./IncidentForm";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+import { IncidentTypeResponse } from "@/lib/issues-difinitions";
 
 
 const IncidentType = () => {
     const [searchText, setSearchText] = useState("");
     const token = useTokenStore().token;
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editRecord, setEditRecord] = useState<Partial<IncidentTypeResponse> | undefined>(undefined);
     const queryClient = useQueryClient();
-    const [messageApi, contextHolder] = message.useMessage();
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [IncidentType, setIncidentType] = useState<IncidentTypeResponse | null>(null);
-    const [pdfDataUrl, setPdfDataUrl] = useState(null);
-    const [isPdfModalOpen, setIsPdfModalOpen] = useState(false);
-    const [pagination, setPagination] = useState({ current: 1, pageSize: 10 });
+    // const [pdfDataUrl, setPdfDataUrl] = useState(null);
+    // const [isPdfModalOpen, setIsPdfModalOpen] = useState(false);
 
-    const { data } = useQuery({
+    const { data: incidentType, refetch: refetchIncidentTypes } = useQuery({
         queryKey: ["incident-type"],
         queryFn: () => getIncidentType(token ?? ""),
     });
 
-    const { data: UserData } = useQuery({
-        queryKey: ['user'],
-        queryFn: () => getUser(token ?? "")
-    })
+    const { data: incidentCategories } = useQuery({
+        queryKey: ["incident-category"],
+        queryFn: () => getIncidentCategory(token ?? ""),
+    });
 
     const deleteMutation = useMutation({
         mutationFn: (id: number) => deleteIncidentType(token ?? "", id),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["incident-type"] });
-            messageApi.success("Incident Type deleted successfully");
+            message.success("Incident Type deleted successfully");
         },
-        onError: (error: any) => {
-            messageApi.error(error.message || "Failed to delete Incident Type");
+        onError: (error) => {
+            message.error(error.message || "Failed to delete Incident Type");
         },
     });
+
+    const showEditModal = (record: Partial<IncidentTypeResponse>) => {
+        setEditRecord(record);
+        setIsModalOpen(true);
+    };
 
     const showModal = () => {
         setIsModalOpen(true);
     };
-    
+
     const handleCancel = () => {
         setIsModalOpen(false);
     };
 
-    const dataSource = data?.results?.map((incident_type, index) => ({
+    const dataSource = incidentType?.results?.map((incident_type, index) => ({
         key: index + 1,
         id: incident_type?.id,
         name: incident_type?.name ?? "N/A",
         description: incident_type?.description ?? "N/A",
-        organization: incident_type?.organization ?? 'Bureau of Jail Management and Penology',
-        updated: `${UserData?.first_name ?? ''} ${UserData?.last_name ?? ''}`,
+        updated_by: incident_type?.updated_by ?? "N/A",
+        category: incident_type?.category ?? null,
     })) || [];
 
     const filteredData = dataSource?.filter((incident_type) =>
@@ -64,10 +68,10 @@ const IncidentType = () => {
         )
     );
 
-    const columns: ColumnsType<IncidentTypeResponse> = [
+    const columns: ColumnsType<{ key: number; name: string; description: string; updated_by: string; }> = [
         {
             title: 'No.',
-            render: (_, __, index) => (pagination.current - 1) * pagination.pageSize + index + 1,
+            render: (_, __, index) => index + 1,
         },
         {
             title: 'Incident Type',
@@ -99,11 +103,107 @@ const IncidentType = () => {
             ],
             onFilter: (value, record) => record.description === value,
         },
+        {
+            title: 'Updated By',
+            dataIndex: 'updated_by',
+            key: 'updated_by',
+            sorter: (a, b) => a.description.localeCompare(b.updated_by),
+            filters: [
+                ...Array.from(
+                    new Set(filteredData.map(item => item.updated_by))
+                ).map(description => ({
+                    text: description,
+                    value: description,
+                }))
+            ],
+            onFilter: (value, record) => record.updated_by === value,
+        },
+        {
+            title: 'Actions',
+            key: 'actions',
+            align: 'center',
+            render: (_, record) => (
+                <div className="flex gap-2 w-full items-center justify-center">
+                    <Button
+                        className="text-blue-600 border-blue-600"
+                        onClick={() => showEditModal(record)}
+                    >
+                        <AiOutlineEdit />
+                    </Button>
+                    <Button
+                        danger
+                        onClick={() => {
+                            Modal.confirm({
+                                centered: true,
+                                title: 'Are you sure?',
+                                content: `Delete "${record.name}" incident type?`,
+                                okText: 'Yes, delete it',
+                                cancelText: 'Cancel',
+                                onOk: () => {
+                                    const incident = incidentType?.results.find(i => i.name === record.name);
+                                    if (incident?.id) {
+                                        deleteMutation.mutate(incident.id);
+                                    } else {
+                                        message.error("Incident ID not found.");
+                                    }
+                                },
+                            });
+                        }}
+                    >
+                        <AiOutlineDelete />
+                    </Button>
+                </div>
+            ),
+        }
     ]
     return (
-        <div>
-            <Table dataSource={filteredData} columns={columns} />
-        </div>
+        <>
+            <Modal
+                open={isModalOpen}
+                onCancel={handleCancel}
+                onClose={handleCancel}
+                footer={[]}
+                centered
+            >
+                <IncidentForm
+                    editRecord={editRecord}
+                    onClose={handleCancel}
+                    refetchIncidentTypes={refetchIncidentTypes}
+                    incidentCategory={incidentCategories || { count: 0, next: null, previous: null, results: [] }}
+                />
+            </Modal>
+
+            <div className="w-full h-full flex flex-col gap-4 mt-1">
+                <div className="w-full flex justify-between items-center">
+                    <h1 className="font-bold text-xl">Incident Types</h1>
+                    <div className="flex gap-2 items-center">
+                        <Input
+                            placeholder="🔍 Search incident types..."
+                            value={searchText}
+                            onChange={e => setSearchText(e.target.value)}
+                            allowClear
+                            className="w-60 h-10"
+                        />
+                        <Button
+                            type="primary"
+                            className="h-10"
+                            onClick={showModal}
+                        >
+                            <span className="flex gap-1 items-center">
+                                <Plus />
+                                <span className="font-semibold">
+                                    Add Incident Type
+                                </span>
+                            </span>
+                        </Button>
+                    </div>
+                </div>
+                <Table
+                    dataSource={filteredData}
+                    columns={columns}
+                />
+            </div>
+        </>
     )
 }
 
