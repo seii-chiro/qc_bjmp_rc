5a13e6ee743b7c624032859414facbb9ee345884
John Rhay <javier.johnrhay@gmail.com>
2025-06-03 09:14:39 +0800

    fix: re-add oasis link. Who tf removed it anyway?

--- src/pages/Incidents/Incidents.tsx
+++ src/pages/Incidents/Incidents.tsx
@@ -2,8 +2,8 @@ import { GodotLink, Header } from "../assets/components/link"
 
 const Incidents = () => {
     return (
-        <div className="">
-            <div className="border border-gray-200 py-5 px-10 w-full md:w-96 shadow-sm hover:shadow-md rounded-md text-gray-700">
+        <div className="flex gap-4">
+            <div className="border border-gray-200 py-5 px-10 w-full md:w-fit shadow-sm hover:shadow-md rounded-md text-gray-700">
                 <Header title="INCIDENTS" />
                 <div className="ml-8 mt-2">
                     <GodotLink link="report" title="Report" />
@@ -14,6 +14,12 @@ const Incidents = () => {
                     {/* <GodotLink link="" title="Reporters" />
                     <GodotLink link="" title="Resolvers" />
                     <GodotLink link="" title="Responders" /> */}
+                </div>
+            </div>
+            <div className="border border-gray-200 py-5 px-10 w-full md:w-fit shadow-sm hover:shadow-md rounded-md text-gray-700">
+                <Header title="Alerts and Notifications" />
+                <div className="ml-8 mt-2">
+                    <GodotLink link="oasis" title="Send an Alert" />
                 </div>
             </div>
         </div>
