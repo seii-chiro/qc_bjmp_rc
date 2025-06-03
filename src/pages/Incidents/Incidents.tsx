4463b7d3c42f28cfe36a4bb34e9fe1bef2f0b7cc
John Rhay <javier.johnrhay@gmail.com>
2025-06-03 10:45:06 +0800

    feat: add initial links

--- src/pages/Incidents/Incidents.tsx
+++ src/pages/Incidents/Incidents.tsx
@@ -1,3 +1,4 @@
+import { LuBellRing } from "react-icons/lu"
 import { GodotLink, Header } from "../assets/components/link"
 
 const Incidents = () => {
@@ -17,9 +18,36 @@ const Incidents = () => {
                 </div>
             </div>
             <div className="border border-gray-200 py-5 px-10 w-full md:w-fit shadow-sm hover:shadow-md rounded-md text-gray-700">
-                <Header title="Alerts and Notifications" />
-                <div className="ml-8 mt-2">
-                    <GodotLink link="oasis" title="Send an Alert" />
+                <div>
+                    <div className="flex items-center">
+                        <LuBellRing className="text-gray-600" />
+                        <h1 className="font-bold text-lg ml-4">ALERTS AND NOTIFICATION</h1>
+                    </div>
+                </div>
+                <div className="ml-8 mt-2 flex gap-10">
+                    <div>
+                        <GodotLink link="oasis" title="Send an Alert" />
+                        <GodotLink link="oasis/status" title="Status" />
+                        <GodotLink link="oasis/message_type" title="Message Type" />
+                        <GodotLink link="oasis/scope" title="Scope" />
+                        <GodotLink link="oasis/restriction" title="Restriction" />
+                        <GodotLink link="oasis/code" title="Code" />
+                        <GodotLink link="oasis/note" title="Note" />
+                        <GodotLink link="oasis/language" title="Language" />
+                        <GodotLink link="oasis/category" title="Category" />
+                        <GodotLink link="oasis/event" title="Event" />
+                    </div>
+                    <div>
+                        <GodotLink link="oasis/response_type" title="Response Type" />
+                        <GodotLink link="oasis/urgency" title="Urgency" />
+                        <GodotLink link="oasis/severity" title="Severity" />
+                        <GodotLink link="oasis/certainty" title="Certainty" />
+                        <GodotLink link="oasis/audience" title="Audience" />
+                        <GodotLink link="oasis/event_code" title="Event Code" />
+                        <GodotLink link="oasis/instructions" title="Instructions" />
+                        <GodotLink link="oasis/parameter" title="Parameter" />
+                        <GodotLink link="oasis/geocode" title="Geocode" />
+                    </div>
                 </div>
             </div>
         </div>
