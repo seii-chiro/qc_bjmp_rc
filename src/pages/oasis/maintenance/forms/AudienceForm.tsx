95892eec1b29b1b6ff530f515b046f23324767e0
John Rhay <javier.johnrhay@gmail.com>
2025-06-04 21:10:57 +0800

    feat: add complete audeince table and form

--- src/pages/oasis/maintenance/forms/AudienceForm.tsx
+++ src/pages/oasis/maintenance/forms/AudienceForm.tsx
@@ -1,7 +1,12 @@
+import { useTokenStore } from "@/store/useTokenStore";
 import { AudienceDataSourceRecord } from "../Audience";
+import { useEffect, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Button, Input, message } from "antd";
+import { patchOASISAudience, postOASISAudience } from "@/lib/oasis-query";
 
 export type AudienceFormType = {
-    code: string;
+    audience_text: string;
     description: string;
 };
 
@@ -10,10 +15,73 @@ type Props = {
     handleClose: () => void;
 };
 
-const AudienceForm = ({ }: Props) => {
+const AudienceForm = ({ handleClose, recordToEdit }: Props) => {
+    const token = useTokenStore(state => state.token);
+    const queryClient = useQueryClient();
+    const [form, setForm] = useState<AudienceFormType>({
+        audience_text: "",
+        description: "",
+    });
+
+    useEffect(() => {
+        if (recordToEdit) {
+            setForm({
+                audience_text: recordToEdit.audience_text,
+                description: recordToEdit.description,
+            });
+        }
+    }, [recordToEdit]);
+
+    const mutation = useMutation({
+        mutationKey: [recordToEdit ? 'edit' : 'add', 'audience'],
+        mutationFn: () => {
+            if (recordToEdit) {
+                return patchOASISAudience(token ?? "", recordToEdit.id, form);
+            } else {
+                return postOASISAudience(token ?? "", form);
+            }
+        },
+        onSuccess: () => {
+            message.success(`Successfully ${recordToEdit ? "updated" : "added"} audience`);
+            queryClient.invalidateQueries({ queryKey: ['OASIS', 'audience'] });
+            handleClose();
+        },
+        onError: (err) => message.error(err.message.replace(/[{}[\]]/g, ''))
+    });
+
     return (
-        <div>AudienceForm</div>
-    )
+        <div className="w-full flex flex-col gap-4">
+            <h1 className="text-xl font-semibold">{recordToEdit ? "Edit" : "Add"} Audience</h1>
+
+            <form className="w-full flex flex-col gap-4" onSubmit={e => e.preventDefault()}>
+                <div>
+                    <span className="text-lg font-semibold">Audience</span>
+                    <Input
+                        className="h-10"
+                        value={form.audience_text}
+                        onChange={e => setForm(prev => ({ ...prev, audience_text: e.target.value }))}
+                    />
+                </div>
+                <div>
+                    <span className="text-lg font-semibold">Description</span>
+                    <Input.TextArea
+                        className="!h-52"
+                        value={form.description}
+                        onChange={e => setForm(prev => ({ ...prev, description: e.target.value }))}
+                    />
+                </div>
+
+                <div className="w-full flex justify-end">
+                    <Button
+                        onClick={() => mutation.mutate()}
+                        className="w-24 bg-[#1E365D] text-white"
+                    >
+                        {recordToEdit ? "Update" : "Add"}
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default AudienceForm
