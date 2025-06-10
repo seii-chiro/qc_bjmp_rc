31e56d7f13f89d50b44a3924df26fdd5a5b21d73
John Rhay <javier.johnrhay@gmail.com>
2025-06-10 18:24:50 +0800

    style: update ui

--- src/pages/threat/Threat.tsx
+++ src/pages/threat/Threat.tsx
@@ -1,18 +1,22 @@
-import { NavLink } from "react-router-dom"
+import { GodotLink } from "../assets/components/link"
+import { GiWantedReward } from "react-icons/gi";
 
 const Threat = () => {
     return (
         <div className="flex flex-col gap-5 text-gray-700">
             <div className="w-96 border shadow-sm hover:shadow-md border-gray-200 p-5 rounded-md">
-                <NavLink to={"watch-list"}>
-                    <p className="ml-5 text-basee font-medium">Watchlist</p>
-                </NavLink>
+                <div className="flex gap-2 mb-2 items-center">
+                    <GiWantedReward size={20} />
+                    <p className="font-bold text text-lg">THREATS</p>
+                </div>
+                <GodotLink title="Watchlist" link="watch-list" />
+                <GodotLink title="Watchlist Registration" link="watchlist_registration" />
             </div>
-            <div className="w-96 border shadow-sm hover:shadow-md border-gray-200 p-5 rounded-md">
+            {/* <div className="w-96 border shadow-sm hover:shadow-md border-gray-200 p-5 rounded-md">
                 <NavLink to={"watchlist_registration"}>
                     <p className="ml-5 text-basee font-medium">Watchlist Registration</p>
                 </NavLink>
-            </div>
+            </div> */}
             {/* <div className="w-96 border shadow-sm hover:shadow-md border-gray-200 p-5 rounded-md">
                 <NavLink to={""}>
                     <p className="ml-5 text-basee font-medium">Threat Profile</p>
