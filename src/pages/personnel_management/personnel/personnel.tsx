cdae9e24f5990e7c137928ac30541601d1e21f74
Angella Flogio <flogioangella@gmail.com>
2025-06-11 10:03:45 +0800

    feat: aa change page row

--- src/pages/personnel_management/personnel/personnel.tsx
+++ src/pages/personnel_management/personnel/personnel.tsx
@@ -430,7 +430,7 @@ const handleExportPDF = async () => {
     const doc = new jsPDF('landscape');
     const headerHeight = 48;
     const footerHeight = 32;
-    const maxRowsPerPage = 26;
+    const maxRowsPerPage = 18;
 
     let allData;
     if (searchText.trim() === '') {
