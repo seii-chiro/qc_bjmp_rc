2911cdc406a83d77086bfb8b94b95f34efd29ec8
John Rhay <javier.johnrhay@gmail.com>
2025-06-11 09:15:45 +0800

    feat: add definition file for watchlist maintenance

---
+++ src/lib/watchlist-definitions.d.ts
@@ -0,0 +1,32 @@
+export type WatchlistType = {
+  id: number;
+  created_by: string;
+  updated_by: string;
+  record_status: string;
+  created_at: string;
+  updated_at: string;
+  name: string;
+  description: string;
+};
+
+export type WatchlistRiskLevel = {
+  id: number;
+  created_by: string;
+  updated_by: string;
+  record_status: string;
+  created_at: string;
+  updated_at: string;
+  risk_severity: string;
+  description: string;
+};
+
+export type WatchlistThreatLevel = {
+  id: number;
+  created_by: string;
+  updated_by: string;
+  record_status: string;
+  created_at: string;
+  updated_at: string;
+  threat_level: string;
+  description: string;
+};
